af317a28dd436c1669a658e47e0820f6255e83c0
CaptainIllidan <37904051+CaptainIllidan@users.noreply.github.com>
2019-04-26 09:29:28 +0400

    constraints

--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Routing.Constraints;
+using Microsoft.AspNetCore.Routing;
 
 namespace UrlsAndRoutes
 {
@@ -24,11 +26,19 @@ namespace UrlsAndRoutes
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.UseStatusCodePages();
-            app.UseMvc(routes=>
+            app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "MyRoute",
-                    template: "{controller=Home}/{action=Index}/{id?}/{*catchall}");
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new {controller = "Home", action = "Index"},
+                    constraints: new {
+                        id = new CompositeRouteConstraint(
+                            new IRouteConstraint[] {
+                                new AlphaRouteConstraint(),
+                                new MinLengthRouteConstraint(6)
+                        })
+                    });
             });
         }
     }
