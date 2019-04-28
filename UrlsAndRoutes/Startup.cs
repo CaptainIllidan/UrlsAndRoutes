2a7f1f781ed87562001217000691b8071cbe7427
Cpt.illidan <captain.illidan@yandex.ru>
2019-04-28 19:14:02 +0400

    488

--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -19,7 +19,11 @@ namespace UrlsAndRoutes
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<RouteOptions>(options =>
-                options.ConstraintMap.Add("weekday", typeof(WeekDayConstraint)));
+            {
+                options.ConstraintMap.Add("weekday", typeof(WeekDayConstraint));
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = true;
+            });
             services.AddMvc();
         }
 
